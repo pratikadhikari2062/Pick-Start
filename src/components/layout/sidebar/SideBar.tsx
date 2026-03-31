329381074f8408356dfb9f4c2fa93c9a6a118f40
pratikadhikari2062 <adhikaripratik522@gmail.com>
2026-03-31 20:03:49 +0545

    botton add

--- src/components/layout/sidebar/SideBar.tsx
+++ src/components/layout/sidebar/SideBar.tsx
@@ -1,8 +1,8 @@
 const SideBar: React.FC = () => {
     return (
         <div>
-            <h1>navbar</h1>
-            <h1>about</h1>
+            <button>navbar</button>
+            <button>about</button>
             <h1></h1>
         </div>
     )
