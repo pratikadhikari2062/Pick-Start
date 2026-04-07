716187f25ef13a178943b41852ed9a4c896ba65f
pratikadhikari2062 <adhikaripratik522@gmail.com>
2026-04-07 16:18:05 +0545

    add navbar for root page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,47 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { styled } from "styled-components";
 
+export const NavContaine = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 40px;
+`;
 
+export const HeadingTwo = styled.h2`
+  background-color: var(--secondary-color);
+`;
 
-export const Route = createFileRoute('/')({
+const NavList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  gap:70px;
+  `;
+
+const NavItem = styled.li`
+`;
+const Startbutton = styled.button`
+height:30px;
+width:100px;
+background-color: var(--primary-color);
+`;
+
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>this is root page </div>
+  return (
+    <NavContaine>
+      <HeadingTwo>Brand</HeadingTwo>
+      <NavList>
+        <NavItem>Home</NavItem>
+      <NavItem>Feature</NavItem>
+      <NavItem>Pricing</NavItem>
+      <NavItem>contact</NavItem>
+      </NavList>
+      <Startbutton>Get Start</Startbutton>
+      
+    </NavContaine>
+  );
 }
